64796986d1683b7ec9b7e798e327419a6aeb988f
Lois Talagrand <lois.talagrand@gmail.com>
2019-12-19 15:44:02 -1000

    Display actual articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
 	validates :email, presence: true, uniqueness: true
 	has_secure_password
 	has_many :lessons
+
+	def get_username()
+		return email.split("@").first     
+	end
 end
