e133cfae955d06ddbaef40189817b064f678c766
Lois Talagrand <lois.talagrand@gmail.com>
2019-12-19 16:01:45 -1000

    Log in function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,37 @@
 class SessionsController < ApplicationController
 
+	before_action :logged_in_redirect, only: [:new, :create]
+
+	# homepage
 	def index
 		
+	end
+
+
+	def create 
+		user = User.find_by(email: params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id 
+			# flash[:success] = "You have successfully logged in"
+			redirect_to root_path 
+		else
+			# flash[:now] = "Something is wrong with the information you provided"
+			render root_path
+		end
+	end
+
+	def destroy 
+		session[:user_id] = nil
+		# flash[:success] = "You have successfully logged out"
+		redirect_to root_path
+	end
+
+	private 
+	def logged_in_redirect
+		if logged_in?
+			flash[:error] = "You are already logged in"
+			redirect_to root_path
+		end
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
 	get 'text', to: 'texts#index'
 
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
+
 end
