27d795468a93a36aac409e7dd2d521e477dd24f4
Lois Talagrand <lois.talagrand@gmail.com>
2019-12-22 10:45:36 -1000

    Prevent duplicate cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,21 +6,29 @@ class CardsController < ApplicationController
 
 	# create a new card
 	def create
-		@card = Card.new(
-			user_id: current_user.id,
-			stage: params[:stage], 
-			source: params[:source],
-			timesreviewed: params[:timesreviewed].to_i,
-			timessuccess: params[:timessuccess].to_i,
-			timesfailed: params[:timesfailed].to_i,
-		)
 
-		if @card.save 
-			puts "The card with source: " + @card.source + " was saved"
-
+		@existingCard = Card.where(user_id: current_user.id, source: params[:source])
+		if(@existingCard.exists?)
+			puts "The card already exists for the current user" 
 		else
-			puts "The card was NOT saved"
+			@card = Card.new(
+				user_id: current_user.id,
+				stage: params[:stage], 
+				source: params[:source],
+				timesreviewed: params[:timesreviewed].to_i,
+				timessuccess: params[:timessuccess].to_i,
+				timesfailed: params[:timesfailed].to_i,
+				)
+
+			if @card.save 
+				puts "The card with source: " + @card.source + " was saved"
+
+			else
+				puts "The card was NOT saved"
+			end
 		end
+
+
 	end	
 
 end
