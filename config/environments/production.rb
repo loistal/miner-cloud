0044b27786836be4355ef8a4b52c1d7bf0b0c552
Lois <lois.talagrand@gmail.com>
2019-12-28 06:31:05 -1000

    Production mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'lois.talagrand@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'lois.talagrand@gmail.com',
+    password:             'nichepursuits10000',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
