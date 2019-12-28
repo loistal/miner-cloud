6f10e260c414c39e0270579aa0fe6a1636b6312e
Lois <lois.talagrand@gmail.com>
2019-12-28 05:57:13 -1000

    Send welcome email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 
 		if @user.save 
+			UserMailer.welcome_email(@user).deliver_now
 			redirect_to text_path
 		else
 			# do something
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -31,9 +33,24 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'lois.talagrand@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'lois.talagrand@gmail.com',
+    password:             'nichepursuits10000',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
