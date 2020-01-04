e20d30ca62e70e55118b226210cb1cdcf966c516
Lois Talagrand <lois.talagrand@gmail.com>
2020-01-04 11:56:41 -1000

    Error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 	helper_method :current_user, :logged_in?
 
+	# users need to be logged in on every single page by default
+	# (see https://stackoverflow.com/questions/5056451/redirect-to-login-page-if-user-not-logged-in)
+	before_action :require_user
+
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
@@ -13,7 +17,7 @@ class ApplicationController < ActionController::Base
 	def require_user
 		if !logged_in?
 			flash[:danger] = "You must be logged in to perform that action"
-			redirect_to login_path
+			redirect_to root_path
 		end
 	end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,33 +1,34 @@
 class SessionsController < ApplicationController
 
 	before_action :logged_in_redirect, only: [:new, :create]
+	skip_before_action :require_user
 
 	# homepage
 	def index
-		
+
 	end
 
 
-	def create 
+	def create
 		user = User.find_by(email: params[:session][:email])
 		if user && user.authenticate(params[:session][:password])
-			session[:user_id] = user.id 
+			session[:user_id] = user.id
 			flash[:now] = "You have successfully logged in"
-			redirect_to root_path 
+			redirect_to root_path
 		else
 			puts "FAILED"
 			flash.alert = "Something is wrong with the information you provided"
-			redirect_to text_path
+			redirect_to root_path
 		end
 	end
 
-	def destroy 
+	def destroy
 		session[:user_id] = nil
 		# flash[:success] = "You have successfully logged out"
 		redirect_to root_path
 	end
 
-	private 
+	private
 	def logged_in_redirect
 		if logged_in?
 			flash[:error] = "You are already logged in"
