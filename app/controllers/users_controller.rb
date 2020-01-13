3c3d12392ebf79c4e551d66f7f00b5f582695d2a
Lois <lois.talagrand@gmail.com>
2020-01-12 15:05:36 -1000

    Fix responsive homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 
-	def create 
+
+	def create
 
 		@user = User.new(user_params)
 
-		if @user.save 
+		if @user.save
 
 			begin
 				UserMailer.welcome_email(@user).deliver_now
@@ -30,19 +31,19 @@ class UsersController < ApplicationController
 
 	end
 
-	def account 
+	def account
 		@indexAt = current_user.email.index("@")
 		@username = current_user.email[0, @indexAt]
 
 		@total_sentences = Card.where(user_id: current_user.id).count
-		@number_of_texts = Article.where(user_id: current_user.id).count 
+		@number_of_texts = Article.where(user_id: current_user.id).count
 		@long_term_mem = Card.where("stage >= 8 AND stage < 10 AND user_id = ?", current_user.id).count
 		@short_term_mem = Card.where("stage >= 0 AND stage < 8 AND user_id = ?", current_user.id).count
 	end
 
-	private 
+	private
 
-	def user_params 
+	def user_params
 		params.require(:user).permit(:email, :password)
 	end
 
